ab8dbce4cc1b1a5dea079e2ff34d9d8503e9417d
Marco Carmona <elbuki@users.noreply.github.com>
2022-09-13 16:16:31 -0600

    Initial Commit

---
+++ TwoWayBinding/TwoWayBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingApp.swift
+//  TwoWayBinding
+//
+//  Created by Marco Carmona on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
